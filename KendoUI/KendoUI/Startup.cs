c9b671fa2a7e81ab2da9adffd12642adfc3349ac
pardhakuchipudi <saradhi.kuchipudi@gmail.com>
2016-02-06 11:34:19 +0800

    Entire Solution Push

---
+++ KendoUI/KendoUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KendoUI.Startup))]
+namespace KendoUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
